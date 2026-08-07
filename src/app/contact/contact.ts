ccea57aac7fd40ece3951c6d5cd10e22a9202a0c
Jonas Nell <jonasnell1705@gmail.com>
2026-08-07 15:54:41 +0200

    feat: added basic contact section

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,9 +1,55 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
     selector: 'app-contact',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './contact.html',
     styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+    form: FormGroup;
+
+    submitted = false;
+
+    constructor(private fb: FormBuilder){
+        this.form = this.fb.group({
+            name: ['', [Validators.required, Validators.minLength(3)]],
+            email: ['', [Validators.required, Validators.email]],
+            message: ['', [Validators.required, Validators.minLength(10)]],
+            privacyAccepted: [false, Validators.requiredTrue],
+        });
+    }
+
+    get name() {
+        return this.form.get('name')!;
+    }
+
+    get email(){
+        return this.form.get('email')!;
+    }
+
+    get message(){
+        return this.form.get('message')!;        
+    }
+
+    get privacyAccepted(){
+        return this.form.get('privacyAccepted')!;
+    }
+    
+    onSubmit(){
+        this.submitted = true;
+
+        if (this.form.invalid){
+            this.form.markAllAsTouched();
+            return;
+        }
+        // send email later:
+        console.log('Placeholder: Form values:', this.form.value);
+
+        this.form.reset();
+        this.submitted = false;
+    }
+
+}
