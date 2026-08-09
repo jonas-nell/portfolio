65d7835eff42b8d86fd077725b1740b7da04127d
Jonas Nell <jonasnell1705@gmail.com>
2026-08-09 12:22:18 +0200

    feat: implemented routing -> priv/legal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./home/home').then(m => m.Home) },
+    { path: 'legal-notice', loadComponent: () => import('./pages/legal-notice/legal-notice').then(m => m.LegalNotice) },
+    { path: 'privacy-policy', loadComponent: () => import('./pages/privacy-policy/privacy-policy').then(m => m.PrivacyPolicy) },
+    { path: '**', redirectTo: ''}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
-import { Hero } from "./hero/hero";
-import { AboutMe } from "./about-me/about-me";
-import { SectionIntermission } from "./shared/section-intermission/section-intermission";
-import { Skills } from "./skills/skills";
-import { Portfolio } from "./portfolio/portfolio";
-import { References } from "./references/references";
-import { Contact } from "./contact/contact";
 import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, AboutMe, SectionIntermission, Skills, Portfolio, References, Contact, Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
